7d107568a2bf0d871c04f878d3bad54c0670b5b0
kevin caballero <cab@kevins-MacBook-Pro.local>
2019-12-12 22:08:38 -0800

    final

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ let cityObj = [];
 let cityForecast = [];
 let capCity;
 let capFcast;
+let validChar = true;
 
 let currentPosition = 0;
 let deleteCity = document.getElementById('delete');
-let getW = document.getElementById('search');
+let search = document.getElementById('search');
 let url_pt1 = "https://api.openweathermap.org/data/2.5/weather?q=";
 let url_forecast = "https://api.openweathermap.org/data/2.5/forecast?q=";
 let url_city_pt2 = "";
@@ -47,7 +48,8 @@ let date5 = document.getElementById('date5');
 let date5Low = document.getElementById('date5Low');
 let date5High = document.getElementById('date5High');
 
-// If items stored in localStorage
+//If items stored in localStorage
+//Load saved current day forecast object
 if (localStorage.getItem('myObj')) {
     cityObj = JSON.parse(localStorage.getItem('myObj'));
 
@@ -56,6 +58,7 @@ if (localStorage.getItem('myObj')) {
     }
 }
 
+//Load saved 5 day forecast object
 if (localStorage.getItem('myForecast')) {
     cityForecast = JSON.parse(localStorage.getItem('myForecast'));
 
@@ -66,6 +69,7 @@ if (localStorage.getItem('myForecast')) {
 
 //-------------Add Event Listeners------------//
 
+//Deletes the city currently displayed on screen
 deleteCity.addEventListener('click', function (e) {
     if (cityObj.length > 0) {
         cityObj.splice(currentPosition, 1);
@@ -85,13 +89,25 @@ deleteCity.addEventListener('click', function (e) {
     }
 });
 
-getW.addEventListener('click', function (e) {
+//Initiates search through API data for city weather
+search.addEventListener('click', function (e) {
+    
+    //Checks if user input is ONLY letters
+    allLetter(cityInput, e);
+
+    //If characters other than letters are detected, function will NOT continue
+    if(validChar === false){
+        validChar = true;
+        return;
+    }
+
+    //Checks for duplicate Cities, will alert if there is. If not, continue
     if (cityObj.length > 0) {
         capFcast = cityInput.value.toUpperCase();
         for (let i = 0; i < cityObj.length; i++) {
             capCity = cityObj[i].name.toUpperCase();
             if (capCity === capFcast) {
-                alert("city exist");
+                alert("City exist already, please enter another!");
                 cityInput.value = "";
                 return;
             } else {
@@ -102,17 +118,19 @@ getW.addEventListener('click', function (e) {
         url_city_pt2 = cityInput.value;
     }
 
-
-
-
-
+    //If pass all checks, make FULL API keys
     let fullURL = url_pt1 + url_city_pt2 + url_temp_pt3 + url_key_pt4;
     let fullURL_forecast = url_forecast + url_city_pt2 + url_temp_pt3 + url_key_pt4;
+
+    //Call JSON
     loadJSON(fullURL);
     loadJSONForecast(fullURL_forecast);
+
+    //Clear InputBox field
     cityInput.value = "";
 });
 
+//Displays next city on screen
 next.addEventListener('click', function (e) {
     if (cityObj.length < 1) {
         alert("No cities available!");
@@ -128,6 +146,7 @@ next.addEventListener('click', function (e) {
     }
 });
 
+//Displays previous city on screen
 previous.addEventListener('click', function (e) {
     if (cityObj.length < 1) {
         alert("No cities available!");
@@ -145,6 +164,7 @@ previous.addEventListener('click', function (e) {
 
 //---------Load Your JSON Weather File--------//
 
+//Pulls weather data for current day
 function loadJSON(url) {
     let xmlhttp = new XMLHttpRequest();
 
@@ -158,6 +178,7 @@ function loadJSON(url) {
     xmlhttp.send();
 }
 
+//Pulls weather data for next five days
 function loadJSONForecast(url) {
     let xmlhttp = new XMLHttpRequest();
 
@@ -165,12 +186,13 @@ function loadJSONForecast(url) {
         if (this.readyState == 4 && this.status == 200) {
             let myArr = JSON.parse(this.responseText);
             getForecast(myArr);
-        }
+        } 
     };
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 }
 
+//Push current weather data to cityObj array
 function getWeather(info) {
     cityObj.push(info);
     console.log(cityObj);
@@ -178,8 +200,10 @@ function getWeather(info) {
 
     if (currentPosition === 0) {
         currentDisplay();
-    }
+    } 
 }
+
+//push forecast weather data to forecastObj array
 function getForecast(info) {
     cityForecast.push(info);
     console.log(cityForecast);
@@ -190,11 +214,13 @@ function getForecast(info) {
     }
 }
 
+//Save to localStorage
 function saveData() {
     localStorage.setItem('myObj', JSON.stringify(cityObj));
     localStorage.setItem('myForecast', JSON.stringify(cityForecast));
 }
 
+//Function to display current weather
 function currentDisplay() {
     currentCity.innerText = cityObj[currentPosition].name;
     currentTemp.innerText = Math.round(cityObj[currentPosition].main.temp);
@@ -202,33 +228,35 @@ function currentDisplay() {
     currentHigh.innerText = Math.round(cityObj[currentPosition].main.temp_max);
 }
 
+//Function to display forecast weather
 function forecastDisplay() {
     day1pic.setAttribute('src', 'http://openweathermap.org/img/wn/' + cityForecast[currentPosition].list[0].weather[0].icon + '@2x.png');
     date1.innerText = getDate(0);
     date1Low.innerText = Math.round(cityForecast[currentPosition].list[0].main.temp_min) + "°F";
     date1High.innerText = Math.round(cityForecast[currentPosition].list[0].main.temp_max) + "°F";
 
-    day2pic.setAttribute('src', 'http://openweathermap.org/img/wn/' + cityForecast[currentPosition].list[7].weather[0].icon + '@2x.png');
-    date2.innerText = getDate(7);
-    date2Low.innerText = Math.round(cityForecast[currentPosition].list[7].main.temp_min) + "°F";
-    date2High.innerText = Math.round(cityForecast[currentPosition].list[7].main.temp_max) + "°F";
+    day2pic.setAttribute('src', 'http://openweathermap.org/img/wn/' + cityForecast[currentPosition].list[11].weather[0].icon + '@2x.png');
+    date2.innerText = getDate(11);
+    date2Low.innerText = Math.round(cityForecast[currentPosition].list[11].main.temp_min) + "°F";
+    date2High.innerText = Math.round(cityForecast[currentPosition].list[11].main.temp_max) + "°F";
 
-    day3pic.setAttribute('src', 'http://openweathermap.org/img/wn/' + cityForecast[currentPosition].list[15].weather[0].icon + '@2x.png');
-    date3.innerText = getDate(15);
-    date3Low.innerText = Math.round(cityForecast[currentPosition].list[15].main.temp_min) + "°F";
-    date3High.innerText = Math.round(cityForecast[currentPosition].list[15].main.temp_max) + "°F";
+    day3pic.setAttribute('src', 'http://openweathermap.org/img/wn/' + cityForecast[currentPosition].list[19].weather[0].icon + '@2x.png');
+    date3.innerText = getDate(19);
+    date3Low.innerText = Math.round(cityForecast[currentPosition].list[19].main.temp_min) + "°F";
+    date3High.innerText = Math.round(cityForecast[currentPosition].list[19].main.temp_max) + "°F";
 
-    day4pic.setAttribute('src', 'http://openweathermap.org/img/wn/' + cityForecast[currentPosition].list[23].weather[0].icon + '@2x.png');
-    date4.innerText = getDate(23);
-    date4Low.innerText = Math.round(cityForecast[currentPosition].list[23].main.temp_min) + "°F";
-    date4High.innerText = Math.round(cityForecast[currentPosition].list[23].main.temp_max) + "°F";
+    day4pic.setAttribute('src', 'http://openweathermap.org/img/wn/' + cityForecast[currentPosition].list[27].weather[0].icon + '@2x.png');
+    date4.innerText = getDate(27);
+    date4Low.innerText = Math.round(cityForecast[currentPosition].list[27].main.temp_min) + "°F";
+    date4High.innerText = Math.round(cityForecast[currentPosition].list[27].main.temp_max) + "°F";
 
-    day5pic.setAttribute('src', 'http://openweathermap.org/img/wn/' + cityForecast[currentPosition].list[31].weather[0].icon + '@2x.png');
-    date5.innerText = getDate(31);
-    date5Low.innerText = Math.round(cityForecast[currentPosition].list[31].main.temp_min) + "°F";
-    date5High.innerText = Math.round(cityForecast[currentPosition].list[31].main.temp_max) + "°F";
+    day5pic.setAttribute('src', 'http://openweathermap.org/img/wn/' + cityForecast[currentPosition].list[35].weather[0].icon + '@2x.png');
+    date5.innerText = getDate(35);
+    date5Low.innerText = Math.round(cityForecast[currentPosition].list[35].main.temp_min) + "°F";
+    date5High.innerText = Math.round(cityForecast[currentPosition].list[35].main.temp_max) + "°F";
 }
 
+//Function to pull MONTH & DAY ONLY
 function getDate(dayPosition) {
     let fullDate = cityForecast[currentPosition].list[dayPosition].dt_txt;
     let actualDate = "";
@@ -236,4 +264,14 @@ function getDate(dayPosition) {
     actualDate = fullDate[5] + fullDate[6] + "/" + fullDate[8] + fullDate[9]
 
     return actualDate;
+}
+
+//Function to check if user is ONLY typing in Letters, and white space/ " " if followed by a LETTER
+function allLetter(inputtxt, ev) {
+    let letters = /^[a-zA-Z\s-, ]+$/;
+    if (!inputtxt.value.match(letters)) {
+        alert('Please input letters only');
+        cityInput.value = "";
+        validChar = false;
+    }
 }
